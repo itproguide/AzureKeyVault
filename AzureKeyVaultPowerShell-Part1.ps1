624e83acaf751d5b66805f006395834d0a5f1d7b
Mohammed Niyas <43110927+itproguide@users.noreply.github.com>
2021-07-20 23:15:16 +0400

    UpdateAzureKeyVaultPowerShell-Part1
    Updated with Import commands

--- AzureKeyVaultPowerShell-Part1.ps1
+++ AzureKeyVaultPowerShell-Part1.ps1
@@ -30,10 +30,49 @@ $KeyUsage2 = "KeyCertSign"
 # Setup the policy to create a certificate
 $Policy = New-AzKeyVaultCertificatePolicy -SecretContentType "application/x-pkcs12" -SubjectName $SubjectName -IssuerName $IssuerName -ValidityInMonths $validity -ReuseKeyOnRenewal -KeySize $KeySize -RenewAtNumberOfDaysBeforeExpiry $RenewDays -CertificateTransparency 0 -DnsName $DnsName -KeyType $KeyType -Ekus $oid1,$oid1 -KeyUsage $KeyUsage1,$KeyUsage2
 
-$nameofcert="MYCERT"
+$nameofcert="MYCERT1"
 # Creat a certificate
-Add-AzKeyVaultCertificate -VaultName $keyVaultName -Name $nameofcert -CertificatePolicy $Policy
+$certificateOps = Add-AzKeyVaultCertificate -VaultName $keyVaultName -Name $nameofcert -CertificatePolicy $Policy
 
+#PrintCSR
+$certificateOps.CertificateSigningRequest
+
+#Create a file for CSR
+$csrFile = "C:\cert\cert1.csr"
+
+#Add content to csr format
+Add-Content -Path $csrFile -Value "-----BEGIN CERTIFICATE REQUEST-----" -Encoding Ascii
+Add-Content -Path $csrFile -Value $certRequest.CertificateSigningRequest -Encoding Ascii
+Add-Content -Path $csrFile -Value "-----END CERTIFICATE REQUEST-----" -Encoding Ascii
+
+########################
+# copy the csr, Sign a CA using CSR. 
+# import to CA 
+########################
+
+
+$crtFile = "C:\cert\cert1.cer"
+$Name = "MYCERT1"
+#Import certificate to the keyvault
+Import-AzKeyVaultCertificate -VaultName $keyVaultName -Name $Name -FilePath $crtFile
+
+#Certificate Status
 Get-AzKeyVaultCertificate -VaultName $keyVaultName -Name $nameofcert
 
+########################
+# import a pfx certificate 
+########################
+
+$pfxPassword = "123"
+
+$Password = ConvertTo-SecureString -String $pfxPassword -AsPlainText -Force
+$filePath = "C:\cert\MyCertificate.pfx"
+$Name = "m365proguide"
+Import-AzKeyVaultCertificate -VaultName $keyVaultName -Name $Name -FilePath $filePath -Password $Password
+Get-AzKeyVaultCertificate -VaultName $keyVaultName -Name $Name
+########################
+# remove the resource group  
+########################
+
+#Remove Azure Resource Group
 Remove-AzResourceGroup -Name "$ResourceGroup"
